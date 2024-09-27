080a7a9c86357f9ae9383c78e28c737129b541d0
Billy Farrell <billyfarrell95@gmail.com>
2024-09-27 07:53:37 -0500

    icon path update

--- src/components/StepFive/StepFive.tsx
+++ src/components/StepFive/StepFive.tsx
@@ -1,10 +1,11 @@
 import "./StepFive.css";
+import thankYouIcon from "../../assets/images/icon-thank-you.svg"
 
 function StepFive() {
 
   return (
     <div className="step-five-wrapper">
-        <img src="../../src/assets/images/icon-thank-you.svg" alt="" />
+        <img src={thankYouIcon} alt="" />
         <h2>Thank you!</h2>
         <p>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</p>
     </div>
